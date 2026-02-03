63a8d2634ef3b20bd3bf9e4df648ec5138b89156
privatemusk550-glitch <privatemusk550@gmail.com>
2026-02-03 14:01:13 +0100

    Initial commit

---
+++ docs/sandbox.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dropMenu = document.getElementById('drop-menu' );
+const moblieMenu = document.getElementById('moblie-menu' );
+const trackingInput = document.getElementById('tracking-input');
+const mainContianer = document.querySelector('#main-container');
+const loading = document.getElementById('loading')
+const error = document.getElementById('error');
+const form = document.querySelector('form')
+
+
+moblieMenu.addEventListener('click', () => {
+    dropMenu.classList.toggle('hidden');
+    dropMenu.classList.toggle('block')
+//  function replacePage(e){
+    
+//  }
+})
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log(trackingInput)
+    if (trackingInput.value === "RR348294923890FL") {
+        mainContianer.classList.add('hidden')
+        loading.classList.add('flex')
+        loading.classList.remove('hidden')
+      setTimeout(() => {
+  window.location.replace("./RR348294923890FL.html");
+}, 3000);
+
+    }
+    else{
+        trackingInput.classList.add('border-red-600');
+        trackingInput.classList.add('outline-red')
+        error.classList.remove('hidden')
+        error.classList.add('block')
+    }
+})
+})
+
